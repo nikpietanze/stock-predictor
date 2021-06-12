a745dc028bb29fd01f9e484fa6b0a7689f85f34f
Nik Pietanze <nikpietanze@gmail.com>
2021-06-11 22:25:04 -0400

    add stocks list and chart plotting

--- main.py
+++ main.py
@@ -1,21 +1,62 @@
 from stocks import Stock
 import pandas as pd
+import matplotlib.pyplot as plt
 import os.path
 import json
 
+stocks_of_interest = {
+  'GME': Stock('GME', 'US'),
+  'AMC': Stock('AMC', 'US'),
+  'BB': Stock('BB', 'US'),
+  'TSLA': Stock('TSLA', 'US'),
+  'AAPL': Stock('AAPL', 'US'),
+  'NVDA': Stock('NVDA', 'US'),
+  'MSFT': Stock('MSFT', 'US'),
+  'AMZN': Stock('AMZN', 'US'),
+  'NET': Stock('NET', 'US'),
+  'TECH': Stock('TECH', 'US'),
+  'TME': Stock('TME', 'US'),
+  'PINS': Stock('PINS', 'US'),
+  'CAG': Stock('CAG', 'US'),
+  'ICLN': Stock('ICLN', 'US'),
+  'PLTR': Stock('PLTR', 'US'),
+  'NIO': Stock('NIO', 'US'),
+  'TLRY': Stock('TLRY', 'US'),
+  'BABA': Stock('BABA', 'US'),
+  'ACB': Stock('ACB', 'US'),
+  'ARKG': Stock('ARKG', 'US'),
+  'MT': Stock('MT', 'US'),
+  'WISH': Stock('WISH', 'US'),
+  'QS': Stock('QS', 'US'),
+  'BBBY': Stock('BBBY', 'US'),
+  'TAN': Stock('TAN', 'US'),
+  'GLD': Stock('GLD', 'US'),
+  'FB': Stock('FB', 'US'),
+  'SQ': Stock('SQ', 'US'),
+  'QQQ': Stock('QQQ', 'US'),
+  'SLV': Stock('SLV', 'US'),
+  'AMD': Stock('AMD', 'US'),
+  'DKNG': Stock('DKNG', 'US'),
+  'CAG': Stock('CAG', 'US')
+}
 
-GME = Stock('GME', 'US')
+for s in stocks_of_interest:
+  stock = stocks_of_interest[s]
+  if (not os.path.isfile('./data/' + stock.ticker + '.json') or not stock.cached()):
+    stock_data = stock.getStockData()
 
-if (not os.path.isfile('./data/' + GME.ticker + '.json') or not GME.cached()):
-  print('File doesn\'t exist or cache is expired...updating file')
-  stock_data = GME.getStockData()
+    with open('./data/' + stock.ticker + '.json', 'w') as f:
+      f.write(stock_data + '\n')
 
-  with open('./data/' + GME.ticker + '.json', 'w') as f:
-    f.write(stock_data + '\n')
+  f = open('./data/' + stock.ticker + '.json')
+  json_data = json.load(f)
 
-f = open('./data/' + GME.ticker + '.json')
-GME_json_data = json.load(f)
+  data = pd.json_normalize(json_data)
+  df = pd.DataFrame(data['prices'][0])
 
-GME_data = pd.json_normalize(GME_json_data)
-df = pd.DataFrame(GME_data['prices'][0])
-print(df)
+  data_high = df.drop(columns=['open', 'low', 'close', 'volume', 'adjclose'])
+  plt.plot(data_high.date, data_high.high, marker='o')
+  plt.xlabel('Date')
+  plt.ylabel('Price')
+  plt.title(stock.ticker + ' Daily Highs')
+  plt.show()
